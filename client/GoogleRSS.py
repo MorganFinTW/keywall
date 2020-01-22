e4fc412069cf685d5c605cc2787b16c8e7f85c22
MorganFC3 <mrfunctionc3@gmail.com>
2020-01-22 18:00:05 +0800

    implement the save function to store results

--- client/GoogleRSS.py
+++ client/GoogleRSS.py
@@ -6,7 +6,10 @@ from client.base import Client
 
 
 class GoogleRSS(Client):
-
+    """
+    Grep Google RSS content, and transform into tokens
+    """
+    #
     url = 'https://news.google.com/rss?hl=zh-TW&gl=TW&ceid=TW:zh-Hant'
 
     def __init__(self, *args, **kwargs):
@@ -14,14 +17,30 @@ class GoogleRSS(Client):
 
     def run(self):
 
-        rss_resp = self.get_rss()
-        if rss_resp:
-            feed = self.serializer(rss_resp.content)
-            for post in feed.items:
-                self._logger.info("post summary: " + post.description)
+        self.raw = self.get_rss()
+
+        if self.raw:
+            feed = self.serializer(self.raw)
+            if feed.items:
+                (self.content, self.tokens) = self.get_description_content_and_tokens(
+                    feed.items)
+
+        if self.is_save:
+            self.save()
+
+    def get_description_content_and_tokens(self, feed_items):
+        content, tokens = "", ""
+        for post in feed_items:
+            content += "%s\n" % post.description
+            self._logger.debug("post summary: " + post.description)
+
+            # TODO: need tokenize post.description
+            tokens
+
+        return content, tokens
 
     def serializer(self, content: bytes):
-        feed = {}
+        feed = None
         try:
             feed = atoma.parse_rss_bytes(content)
         except Exception:
@@ -29,11 +48,11 @@ class GoogleRSS(Client):
 
         return feed
 
-    def get_rss(self):
+    def get_rss(self) -> bytes:
         response = None
         try:
             response = requests.get(self.url)
         except RequestException as e:
             self._logger.error("requests fail: %s" % e)
 
-        return response
+        return response.content
--- client/base.py
+++ client/base.py
@@ -1,10 +1,27 @@
+from utility.utils import save_text_to_file
+
 
 class Client:
 
+    raw = None
+    content = None
+    tokens = None
+
     def __init__(self, *args, logger=None, save=False):
         self._logger = logger
-        self._save = save
+        self.is_save: bool = save
 
     def run(self):
         raise NotImplementedError
+
+    def save(self):
+        data_list = [
+            ('./output/q1/', 'news.rss', self.raw.decode()),
+            ('./output/q1/', 'description.txt', self.content),
+            ('./output/q1/', 'output.txt', self.content)
+        ]
+
+        for path, name, text in data_list:
+            if text:
+                save_text_to_file(path, name, text)
 
