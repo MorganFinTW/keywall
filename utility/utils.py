a8df4e81d43695ca7a363fd652974e9f238a5489
MorganFC3 <mrfunctionc3@gmail.com>
2020-01-22 17:50:42 +0800

    check path exist or create folder pass save into client

--- utility/utils.py
+++ utility/utils.py
@@ -1,7 +1,22 @@
+import os
 import pydoc
 
 
 def get_client(options, *args, **kwargs):
+
+    if options.save:
+        kwargs['save'] = True
+
     class_ = pydoc.locate("client.%s.%s" % (options.source, options.source))
     _client = class_(*args, **kwargs)
     return _client
+
+
+def save_text_to_file(path: str, file_name: str, text: str):
+    file_path = "%s%s" % (path, file_name)
+
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+    with open(file_path, "w") as f:
+        f.write(text)
